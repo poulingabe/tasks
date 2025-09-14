75be327d5f2619a80af754d411f18b18e22b054c
Gabriel Poulin <gabriel.poulin@maine.edu>
2025-09-14 15:16:39 -0400

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): React.JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Gabriel Poulin
+                automatically reload. Gabriel Poulin Hello World
             </p>
         </div>
     );
